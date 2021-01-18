cc1e837a4f36bf0b2391b1847ca3a5dbd6279411
Kiri M. Kvistnes <71313020+kirikvi@users.noreply.github.com>
2021-01-18 12:55:08 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,30 +9,34 @@ const cat = {
 cat.complain();
 
 
-// Question 2:
-/*Select the h3 from the HTML using the querySelector method and assign it to a variable called heading.
-
-Change its innerHTML value to "Updated heading".*/
+// Question 2
 
 const h3 = document.querySelector("h3");
 
 h3.innerHTML = "Updated heading";
 
+
+// Question 3
+
+h3.style.fontSize = "2em";
+
+
+// Question 4
+
+h3.classList.add("subheading");
+
+
+// Question 5:
+//Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
+//Loop through the p elements and change the colour of each to "red".
+
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i ++){
+    paragraphs[i].style.color = "red";
+}
+
 /*
-Question 3:
-Use the style property on the heading variable from the question above to change its font size to "2em".
-
-
-Question 4:
-Add a class to the heading variable called subheading.
-
-
-Question 5:
-Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
-
-Loop through the p elements and change the colour of each to "red".
-
-
 Question 6:
 Select the div with a class of results, assign it to a variable called resultsContainer and set its inner HTML to be <p>New paragraph</p> and its background colour to be yellow.
 
